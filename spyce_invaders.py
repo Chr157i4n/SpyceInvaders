2760cf321b1b4adf272c2270a1eaf9720c821efc
Christian Köhlke <christian@koehlke.de>
2023-11-21 20:10:14 +0100

    added spaceship image - added ability to move the spaceship around with WASD

--- spyce_invaders.py
+++ spyce_invaders.py
@@ -11,8 +11,17 @@ GREEN   = ( 0, 255, 0)
 BLACK = ( 0, 0, 0)
 WHITE   = ( 255, 255, 255)
 
-screen = pygame.display.set_mode((640, 480))
 pygame.display.set_caption("Spyce Invaders")
+program_icon = pygame.image.load('images/spaceship.png')
+pygame.display.set_icon(program_icon)
+screen = pygame.display.set_mode((640, 480))
+
+
+spaceship_image = pygame.image.load("images/spaceship.png")
+spaceship_image = pygame.transform.scale(spaceship_image, (100, 100))
+spaceship_imagerect = spaceship_image.get_rect()
+
+print(spaceship_imagerect)
 
 clock = pygame.time.Clock()
 
@@ -23,33 +32,21 @@ while RUN_GAME:
         if event.type == pygame.QUIT:
             RUN_GAME = False
             print("Quit button was pressed")
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                print("Arrow right")
-            elif event.key == pygame.K_LEFT:
-                print("Arrow left")
-            elif event.key == pygame.K_UP:
-                print("Arrow up")
-            elif event.key == pygame.K_DOWN:
-                print("Arrow down")
-            elif event.key == pygame.K_SPACE:
-                print("Space")
+        else:
 
-            elif event.key == pygame.K_w:
-                print("W")
-            elif event.key == pygame.K_a:
-                print("A")
-            elif event.key == pygame.K_s:
-                print("S")
-            elif event.key == pygame.K_d:
-                print("D")
-
-        elif event.type == pygame.MOUSEBUTTONDOWN:
-            print("Mouse button")
+            keys=pygame.key.get_pressed()
+            if keys[pygame.K_a]:
+                spaceship_imagerect.x -= 1
+            if keys[pygame.K_d]:
+                spaceship_imagerect.x += 1
+            if keys[pygame.K_w]:
+                spaceship_imagerect.y -= 1
+            if keys[pygame.K_s]:
+                spaceship_imagerect.y += 1            
 
 
     screen.fill(WHITE)
-
+    screen.blit(spaceship_image, spaceship_imagerect)
 
     pygame.display.flip()
 
